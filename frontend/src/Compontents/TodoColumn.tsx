b533ee5f34658243b6d20a2af65b55b1a0d0481e
WeiMing <110397919+WeiMingTay@users.noreply.github.com>
2023-10-06 16:43:33 +0200

    nicht genutzte Zeilen gelöscht

--- frontend/src/Compontents/TodoColumn.tsx
+++ frontend/src/Compontents/TodoColumn.tsx
@@ -1,5 +1,7 @@
 import {Todo, TodoStatus} from "../assets/todos.ts";
 import TodoCard from "./TodoCard.tsx";
+import "../assets/Todo.css"
+
 
 type TodoColumnProps = {
     todos: Todo[],
@@ -11,7 +13,14 @@ export default function TodoColumn(props: TodoColumnProps) {
     return (<>
 
             <section className={"section-status"}>
-                <h2>{props.status.toLowerCase()}</h2>
+
+                {props.status === "OPEN"
+                    ? <h2 className="status-open">{props.status.toLowerCase()}</h2>
+                    : props.status === "IN_PROGRESS"
+                        ? <h2 className="status-inProgress">{props.status.toLowerCase()}</h2>
+                        : <h2 className="status-done">{props.status.toLowerCase()}</h2>
+                }
+
                 {
                     props.todos.map(todo => <TodoCard todo={todo} key={todo.id}
                                                       onTodoItemChange={props.onTodoItemChange}/>)
